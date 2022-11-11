b2f0861e4ddf4c0c5a392a77b2c6c80d7ea9521b
alessiogandelli <alessiogandelli99@gmail.com>
2022-11-11 23:08:50 +0100

    generate sensors

--- src/generator.py
+++ src/generator.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 import datetime
 
+
+
 # age generator 
 def age_generator():
     age = np.random.randint(10, 100)
@@ -18,22 +20,37 @@ def time_generator():
     time = np.random.randint(0, 100)
     return time
 
-
+def get_path():
+    return [0,1,3,2,3,4,0]
 
 
 customers = []
 sensors = []
-for i in range(10):
+for i in range(1):
     now = datetime.datetime.now()
     customers.append({ 'time': now, 'age': age_generator(), 'gender': gender_generator()})
-
     
 
+    p = get_path()
+
+    while len(p) > 0:
+        now = now + datetime.timedelta(seconds=1)
+        if np.random.randint(0, 10) > 5:
+            sensor = p.pop(0)
+            print('sensor', sensor)
+
+        sensors.append({ 'time': now, 'sensor':sensor })
+
+    for sensor in p:
+        wait = np.random.randint(0, 100)
+        now = now + datetime.timedelta(seconds=wait)
+        sensors.append({'time': now , 'sensor_id': sensor})
 
 
-   # sensors.append({ 'time': now, 'id' : 0 , 'power': time_generator() })
+
 
 pd.DataFrame(customers)
+pd.DataFrame(sensors)
     
 
 
