ddf945389716f11af90166004ffe67feae7f929a
alessiogandelli <alessiogandelli99@gmail.com>
2022-11-12 00:14:16 +0100

    add path

--- src/generator.py
+++ src/generator.py
@@ -4,7 +4,35 @@ import time
 import numpy as np
 import pandas as pd
 import datetime
+import csv
+import time
+from random import seed
+from random import randint
+import numpy as np
+import matplotlib.pyplot as plt 
+import networkx as nx
 
+G = nx.Graph()
+
+for i in range(0,9):
+    G.add_node(i)
+
+G.add_edge(0,1)
+G.add_edge(0,2)
+G.add_edge(1,3)
+G.add_edge(2,3)
+G.add_edge(1,4)
+G.add_edge(0,3)
+G.add_edge(1,5)
+G.add_edge(2,5)
+G.add_edge(0,4)
+G.add_edge(5,4)
+G.add_edge(2,7)
+G.add_edge(4,8)
+G.add_edge(1,8)
+G.add_edge(0,7)
+G.add_edge(3,7)
+G.add_edge(3,6)
 
 
 # person generator
@@ -20,9 +48,24 @@ def time_generator():
     time = np.random.randint(0, 100)
     return time
 
-def get_path():
-    return [0,1,3,2,3,4,0]
+def get_path(G):
+    
+    iniz = 0
+    fine = 1
+    current = iniz
+    lista_sensors = [iniz]
 
+    while current != fine:
+        l = [edge  for edge in list(G.edges(iniz)) if edge[1] != iniz ]
+
+        current = l[randint(0,len(l)-1)][1]
+        lista_sensors.append(current)
+        
+    return lista_sensors
+
+
+
+#%%
 
 customers = []
 sensors = []
@@ -35,28 +78,30 @@ n_users = 10 # number of users we want to generate data for
 for i in range(n_users):
     offset = np.random.randint(0, timespan)
     now = today + datetime.timedelta(seconds=offset)
-    customers.append({ 'time': now, 'age': age_generator(), 'gender': gender_generator()})
     address = np.random.randint(100000, 999999)
+    customers.append({ 'time': now, 'age': age_generator(), 'gender': gender_generator(),  'address': address})
+    sensor = 0
+    sensors.append({ 'time': now, 'sensor':sensor , 'address': address}) # entrata 
 
-    p = get_path()
+    p = get_path(G)
 
     while len(p) > 0:
         now = now + datetime.timedelta(seconds=1)
-        if np.random.randint(0, 10) > 7:
+
+        if np.random.randint(0, 10) > 5:
             sensor = p.pop(0)
+
+        sensors.append({ 'time': now, 'sensor':sensor , 'address': address}) # entrata 
+
             
 
-        sensors.append({ 'time': now, 'sensor':sensor , 'address': address})
-
-    for sensor in p:
-        wait = np.random.randint(0, 100)
-        now = now + datetime.timedelta(seconds=wait)
-        sensors.append({'time': now , 'sensor_id': sensor})
+            
 
 
 
 
-pd.DataFrame(customers)
+
+customers = pd.DataFrame(customers)
 sensors = pd.DataFrame(sensors).astype({'time': 'datetime64[ns]'})
 
     
@@ -70,5 +115,16 @@ pd.set_option('display.max_rows', 1000)
 
 
 
+
+
+#%%
+
+
+
+
+
+
+
+#%%
 
 # %%
